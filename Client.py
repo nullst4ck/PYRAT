bab80f84aef5df4dd1b8f28606176ee36e895cae
nullst4ck <nullst4ck@protonmail.com>
2022-04-04 13:29:48 -0700

    Revert "Update Client.py"
    This reverts commit 0f9820dcaa47be6b6707a5c9c92adbfdf3a469d9.

--- Client.py
+++ Client.py
@@ -23,10 +23,10 @@ def validate_ip_address(address):
     print("IP address {} is valid".format(address))
     return True
 
-#Run IP Validation Function1
+#Run IP Validation Function
 validate_ip_address(Server_Host)
 
-#Ask for Server Port1
+#Ask for Server Port
 Server_Port = input("Server port? ")
 
 #Print Server Host and Port
